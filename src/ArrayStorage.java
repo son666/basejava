b6eba5bf55dbea9b361d12eb0f94e805a7930c57
Aleksandr Alpatkin <web_master.07@mail.ru>
2020-03-05 11:28:22 +0300

    refact class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,38 +5,38 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
-    public static void main(String[] args) {
-
-    }
+    private int size = size();
 
     void clear() {
-        if (this.storage[0] != null) {
-            for (int i = this.size() - 1; i >= 0; i--) {
-                this.storage[i] = null;
+        if (storage[0] != null) {
+            for (int i = size - 1; i >= 0; i--) {
+                storage[i] = null;
             }
+            size = size();
         }
     }
 
     void save(Resume r) {
-        this.storage[this.size()] = r;
+        storage[size] = r;
+        size = size();
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < this.size(); i++) {
-            if (this.storage[i].uuid.equals(uuid)) {
-                return this.storage[i];
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < this.size(); i++) {
-            if (this.storage[i].uuid.equals(uuid)) {
-                for (int j = i; j < this.size(); j++) {
-                    this.storage[j] = this.storage[j + 1];
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size; j++) {
+                    storage[j] = storage[j + 1];
                 }
+                size = size();
                 break;
             }
         }
@@ -46,14 +46,14 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(this.storage, this.size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        if (this.storage[0] != null) {
+        if (storage[0] != null) {
             int count = 0;
-            for (int i = 0; i < this.storage.length; i++) {
-                if (this.storage[i] != null) {
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i] != null) {
                     count++;
                 }
                 else { break; }

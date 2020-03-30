dce4cc60d13378ca9d0762c8e6b0803668256a5f
Aleksandr Alpatkin <web_master.07@mail.ru>
2020-03-30 18:13:25 +0300

    refact AbstractStorage.java

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -7,38 +7,38 @@ import com.urise.webapp.model.Resume;
 public abstract class AbstractStorage implements Storage {
 
     public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        checkIndexNotExist(index, resume.getUuid());
+        int index = getIndexNotExist(resume.getUuid());;
         updateElement(index, resume);
     }
 
     public void save(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        checkIndexExist(index, resume.getUuid());
+        int index = getIndexExist(resume.getUuid());
         saveElement(resume, index);
     }
 
     public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        checkIndexNotExist(index, uuid);
+        int index = getIndexNotExist(uuid);
         return getElement(index);
     }
 
     public void delete(String uuid) {
-        int index = getIndex(uuid);
-        checkIndexNotExist(index, uuid);
+        int index = getIndexNotExist(uuid);
         deleteElement(index);
     }
 
-    private void checkIndexNotExist(int index, String uuid) {
+    private int getIndexNotExist(String uuid) {
+        int index = getIndex(uuid);
         if (index < 0) {
             throw new NotExistStorageException(uuid);
         }
+        return index;
     }
-    private void checkIndexExist(int index, String uuid) {
+    private int getIndexExist(String uuid) {
+        int index = getIndex(uuid);
         if (index >= 0) {
             throw new ExistStorageException(uuid);
         }
+        return index;
     }
 
     protected abstract int getIndex(String uuid);
